4385c07327b498411822da054abbdffa5ad91a10
David <david.he07@yahoo.ca>
2020-02-11 12:39:15 -0500

    completed typewriter.js file

--- typewriter.js
+++ typewriter.js
@@ -4,7 +4,6 @@ const delay = num => {
   for (const char of sentence) {
     setTimeout(() => {
       process.stdout.write(char);
-      // <= 1s delay to make it noticeable. Can dial it down later.
     }, num);
     num += 100;
   }
